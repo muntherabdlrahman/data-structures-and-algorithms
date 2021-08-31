ebe5ace975e41dbd19b2d3830fdc17cdc42eb6bd
muntherabdlrahman <munther.abdlrahman@gmail.com>
2021-09-01 00:50:06 +0300

    done

--- 401-code-challenge/tree-fizz-buzz/binaryTree.js
+++ 401-code-challenge/tree-fizz-buzz/binaryTree.js
@@ -1,5 +1,6 @@
 "use strict";
 
+const Node=require('./node')
 
 class BinaryTree {
   constructor() {
@@ -65,6 +66,7 @@ class BinaryTree {
     return maxNumber;
   }
 
+
   breadthFirstSearch(tree) {
     let queue = [tree];
     let result = [];
@@ -86,7 +88,30 @@ class BinaryTree {
   }
 }
 
+let one = new Node(2);
+let two = new Node(7);
+let four = new Node(2);
+let five = new Node(6);
+let three = new Node(5);
+let six = new Node(9);
+let seven = new Node(5);
+let eight = new Node(11);
+let nine = new Node(4);
 
+one.left = two;
+one.right = three;
+two.left = four;
+two.right = five;
+three.right = six;
+five.left = seven;
+five.right = eight;
+six.left = nine;
+
+let tree = new BinaryTree();
+
+tree.root = one;
+
+// console.log(tree.breadthFirstSearch(one))
 
 
 module.exports = BinaryTree;
